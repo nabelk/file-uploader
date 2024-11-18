81ec34e4760b7dfb46681db860d77f60551d9529
Nabil Khalid <holanabilkhalid@gmail.com>
2024-11-18 19:48:29 +0800

    feat: login router && controller

---
+++ controllers/loginController.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+
+const loginAuth = async (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.render("index", { message: info.message });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+};
+
+module.exports = { loginAuth };
