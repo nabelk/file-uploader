d383e22fc88a0c810afa1c5505a5f03911fc8eff
Nabil Khalid <holanabilkhalid@gmail.com>
2024-11-19 21:03:56 +0800

    fix: remove dotenv config, utilize global in main server file

--- config/session.js
+++ config/session.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const expressSession = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("@prisma/client");
